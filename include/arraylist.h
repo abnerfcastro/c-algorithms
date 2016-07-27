9cde1a1468932453b6097f79881655860cef2ccb
Abner <abnerc@br.ibm.com>
2016-07-27 09:26:49 -0300

    Comments to arraylist.h

--- include/arraylist.h
+++ include/arraylist.h
@@ -11,9 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>     // malloc
 
-#define HEAD        0x01
-#define ANYWHERE    0x02
-#define TAIL        0x03
+#define HEAD        0x00000000
+#define TAIL        0x7fffffff
 
 // Structure to hold the information
 typedef struct arraylist_node
@@ -29,10 +28,34 @@ typedef struct _array_list
 
 typedef _array_list* array_list;
 
+/**
+ * Creates an array list and allocates the necessary memory.
+ * 
+ * @param size the initial size of the array list
+ * @return reference to an array_list 
+ */
 array_list create_arraylist(size_t size);
+
+/**
+ * Retrieves node from an array list
+ * 
+ * @param list the list to get the node from
+ * @param index position of the node to be retrieved
+ * @return copy of retrieved node
+ */
 array_node get_node(array_list list, int index);
+
+/**
+ * Adds a node to an array list
+ * 
+ * @param list the list to add the node to
+ * @param node the node to be added
+ * @param position the position where the node will be added
+ */
 void add_node(array_list list, array_node node, int position);
+
 void remove_node(array_list list, int position);
+
 size_t get_size(array_list list);
 
 #endif

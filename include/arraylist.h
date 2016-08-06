aa9fb247c41c86d739c11d5558d02773931c218e
Abner <abnerc@br.ibm.com>
2016-08-06 18:46:10 -0300

    First version of Arraylist finished. Still some improvements to work on.

--- include/arraylist.h
+++ include/arraylist.h
@@ -12,15 +12,18 @@
 #include <stdlib.h>     // malloc, realloc
 #include <string.h>     // memcpy
 
+#include "../include/strings.h"
+
 #define HEAD        0x00000000
 #define TAIL        0x7fffffff
 
-// Structure to hold the information
+// Node definition
 typedef struct arraylist_node
 {
     int data;
 } array_node;
 
+// Array List definition
 typedef struct _array_list
 {
     array_node *array;
@@ -41,7 +44,9 @@ array_list create_arraylist(size_t size);
 void destroy_arraylist(array_list list);
 
 /**
- * Retrieves node from an array list
+ * Retrieves node from an array list.
+ *      if index is less than zero, it will return node at position 0
+ *      if index is out of bounds, it will return node at position size - 1 
  *
  * @param list the list to get the node from
  * @param index position of the node to be retrieved
@@ -60,8 +65,33 @@ array_node get_node(array_list list, int index);
  */
 void add_node(array_list list, array_node node, int position);
 
+/**
+ * Removes a node from an array list.
+ *      if position is negative, it will be set to HEAD
+ *      if position is larger than the size of list, it will be set to TAIL
+ *
+ * @param list the list to remove the node from
+ * @param position the position where node to be removed is (can be HEAD, TAIL or other signed integer)
+ */
 void remove_node(array_list list, int position);
 
+/**
+ * Gets the size of the array list, that is, how many elements it holds.
+ * 
+ *
+ * @param list the array list
+ * @returns the size of the list
+ */
 size_t get_size(array_list list);
+
+/**
+ * Prints an array list.
+ *      This function is customizable.
+ *
+ * @param list the list to be printed
+ * @param separator the way to separate the nodes (e.g "\n", " - ", etc.)
+ * @param to_string_function a function that defines how the array nodes are printed
+ */
+void print_arraylist(array_list list, string separator, string (*to_string_function)(array_node node));
 
 #endif

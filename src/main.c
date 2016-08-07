7671031ef905728b6ae99ef449648696dd85a2eb
Abner Castro <abnerfcastro@gmail.com>
2016-08-06 21:52:56 -0300

    Initial commit on Linked List

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "../include/sorting.h"
 #include "../include/arraylist.h"
+#include "../include/linkedlist.h"
 #include "../include/strings.h"
 
 #define ARRAY_SIZE 5
@@ -16,23 +17,27 @@
 //     return str;
 // }
 
-string print_node(array_node node)
-{
-    string str = malloc(100 * sizeof(char));
-    sprintf(str, "{ Name: %s | Age: %d }", node.name, node.age);
-    return str;
-}
+// string print_node(array_node node)
+// {
+//     string str = malloc(100 * sizeof(char));
+//     sprintf(str, "{ Name: %s | Age: %d }", node.name, node.age);
+//     return str;
+// }
+
+void test_linked_lists();
 
 int main(int argc, char const *argv[]) {
+
+    test_linked_lists();
 
     // int array[ARRAY_SIZE] = {5, 4, 3, 2, 1};
     // print_array(array, (size_t)ARRAY_SIZE);
     // bubble_sort(array, (size_t)ARRAY_SIZE);
     // print_array(array, (size_t)ARRAY_SIZE);
 
-    array_list list = create_arraylist(0);    
+    // array_list list = create_arraylist(0);    
 
-    array_node node;
+    // array_node node;
     // node.data = 0;
 
     // for (int i = 0; i < 10000; i++)
@@ -48,27 +53,27 @@ int main(int argc, char const *argv[]) {
     //     node.data++;
     // }
 
-    node.name = "Abner";
-    node.age = 24;
-    add_node(list, node, HEAD);
+    // node.name = "Abner";
+    // node.age = 24;
+    // add_node(list, node, HEAD);
 
-    node.name = "Deborah";
-    node.age = 23;
-    add_node(list, node, HEAD);
+    // node.name = "Deborah";
+    // node.age = 23;
+    // add_node(list, node, HEAD);
 
-    node.name = "Alex";
-    node.age = 13;
-    add_node(list, node, HEAD);
+    // node.name = "Alex";
+    // node.age = 13;
+    // add_node(list, node, HEAD);
 
-    node.name = "Jack";
-    node.age = 30;
-    add_node(list, node, HEAD);
+    // node.name = "Jack";
+    // node.age = 30;
+    // add_node(list, node, HEAD);
 
     
 
-    print_arraylist(list, "\n", print_node);
+    // print_arraylist(list, "\n", print_node);
 
-    printf("\n\n");
+    // printf("\n\n");
 
     // remove_node(list, HEAD);
     // printf("Size: %d :: Allocated Size: %d\n", get_size(list), list->allocated_size);
@@ -94,4 +99,65 @@ int main(int argc, char const *argv[]) {
     // destroy_arraylist(list);
 
     return 0;
+}
+
+typedef struct point
+{
+    int x;
+    int y;
+} point;
+
+string print_ll_node(list_node node)
+{
+    void *data = node.data;
+    point *p = (point *)data;
+    string str = malloc(100 * sizeof(char));
+    sprintf(str, "(%d, %d)", p->x, p->y);
+    return str;
+}
+
+void test_linked_lists()
+{
+    linked_list list = create_linkedlist();
+
+    for (int i = 0; i < 5; i++)
+    {
+        point *p = malloc(sizeof(point));
+        p->x = 2 * i;
+        p->y = 3 * i;
+
+        list_node node;
+        node.data = p;
+        ll_add_node(list, node);
+    }
+
+    print_linkedlist(list, "\n", print_ll_node);
+    
+    // point p1;
+    // p1.x = 10;
+    // p1.y = 15;
+
+    // point p2;
+    // p2.x = 20;
+    // p2.y = 40;
+    
+    // list_node node;    
+    
+    // node.data = &p1;
+    // ll_add_node(list, node);
+
+    // node.data = &p2;
+    // ll_add_node(list, node);
+    // ll_add_node(list, node);
+    // ll_add_node(list, node);
+    // ll_add_node(list, node);
+
+    list_node tmp = ll_get_node(list, 2);
+    // list_node tmp = *(list->head);
+
+    void *data = tmp.data;    
+    point *p_tmp = (point *)data;
+    printf("%d %d\n", p_tmp->x, p_tmp->y);
+    printf("Size: %d\n", list->size);
+
 }
